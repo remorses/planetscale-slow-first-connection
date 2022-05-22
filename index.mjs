f5bbcc2973bf495ea0b84440e5c338aba4d455fb
remorses <beats.by.morse@gmail.com>
2022-05-22 17:22:13 +0200

    add script

---
+++ index.mjs
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import mysql from 'mysql2'
+
+let c
+
+for (let i = 0; i < 5; i++) {
+    console.time('connect')
+    await new Promise((res, rej) => {
+        c = mysql.createConnection(process.env.DATABASE_URL)
+        c.connect((err) => {
+            if (err) rej(err)
+            else res()
+        })
+    })
+    console.timeEnd('connect')
+}
+for (let j = 0; j < 5; j++) {
+    console.time('query')
+    await new Promise((res, rej) =>
+        c.execute(`SELECT 1`, (err) => {
+            if (err) rej(err)
+            else res()
+        }),
+    )
+    console.timeEnd('query')
+}
